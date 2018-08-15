8f786f0a38aaf9e39d7a1ff03c81bcdd229667e3
Skyler Bala <ssbala@ucdavis.edu>
2018-08-15 13:30:10 -0700

    50% working pushpins

--- apps/static/js/index.js
+++ apps/static/js/index.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
     var formData = $(this).serialize();
 
   });
+
+  $('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+  });
 });
+
+
